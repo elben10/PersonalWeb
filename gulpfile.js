31e27fc50582b5d6eb1e8bb911ba445111e20fd4
Jakob Jul Elben <elbenjakobjul@gmail.com>
2018-08-10 16:36:10 +0200

    Gulp compiles scss and minifies css and js placed in css and js folers.

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,47 @@ gulp.task('vendor', function(){
   .pipe(gulp.dest('./static/vendor/popper.js/js/'))
 })
 
+// Compile SCSS
+gulp.task('css:compile', function() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass.sync({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./static/css'))
+});
+
+// Minify CSS
+gulp.task('css:minify', ['css:compile'], function() {
+  return gulp.src([
+      './static/css/*.css',
+      '!./static/css/*.min.css'
+    ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./static/css'));
+});
+
+// CSS
+gulp.task('css', ['css:compile', 'css:minify']);
+
+// Minify JavaScript
+gulp.task('js:minify', function() {
+  return gulp.src([
+      './static/js/*.js',
+      '!./static/js/*.min.js'
+    ])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./static/js'))
+});
+
+// js
+gulp.task('js', ['js:minify']);
+
 
 // Default task
-gulp.task('default', ['vendor']);
+gulp.task('default', ['css', 'js', 'vendor']);
